44ea56e740d3439b7a1e5ab1130244d4c412bfb3
Mohamed Chaker OUAADDI <med18chaker@gmail.com>
2025-11-23 13:34:17 +0100

    Prepare portfolio for production deployment

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,13 @@ const app = express();
 // use necessary middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+  })
+);
 
-app.use('/uploads', express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
 // define routes
 app.use("/api/admin/", adminRoutes);
